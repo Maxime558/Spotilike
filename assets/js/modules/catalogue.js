6f467fa0e48ca19cb9b0a9a0417a50a40ef58b87
Maxime Lottin <maxime.lottin@outlook.com>
2023-10-16 11:02:56 +0200

    AJoute des covers dans le catologue

--- assets/js/modules/catalogue.js
+++ assets/js/modules/catalogue.js
@@ -1,14 +1,34 @@
 const catalogue = [
-    {
-        titre : "blabla",
-        artiste : "blabla",
-        genre : ["blabla"],
-        annee : 1794,
-        cover : "blabla.jpg",
-        audio : "blabla.mp3",
-        album : "blabla",
-        like : false,
-    }
+  {
+    titre: "Rerun Time",
+    artiste: "Front 242",
+    genre: ["Electronic"],
+    annee: 2023,
+    cover: "front242.jpg",
+    audio: "blabla.mp3",
+    album: "Front 242",
+    like: false,
+  },
+  {
+    titre: "I Could Cry",
+    artiste: "Kiefer",
+    genre: ["Hip Hop, Jazz"],
+    annee: 2023,
+    cover: "Its-ok-bu.jpg",
+    audio: "blabla.mp3",
+    album: " It's Ok, B U",
+    like: false,
+  },
+  {
+    titre: "The Great Escape",
+    artiste: "Boys Like Girls",
+    genre: ["Rock, Pop"],
+    annee: 2023,
+    cover: "boys-like-girls.jpg",
+    audio: "blabla.mp3",
+    album: "Boys Like Girls",
+    like: false,
+  },
 ];
 
-export { catalogue }
+export { catalogue };
